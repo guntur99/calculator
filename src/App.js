f5b82aec40eb86dbb0e7d5c44b904b9e1255ca3c
guntur99 <gugunguntur99@gmail.com>
2022-08-05 22:16:49 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import Wrapper from "./components/Wrapper";
+import Screen from "./components/Screen";
+import ButtonBox from "./components/ButtonBox";
+import Button from "./components/Button";
 
-function App() {
+const btnValues = [
+  ["+", "-"],
+  ["/", "^"],
+  ["SWAP", "*"],
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  ["ON/OFF", 0, "CLEAR"],
+];
+
+const numbArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Screen value="0" />
+      <ButtonBox>
+        {
+          btnValues.flat().map((btn, i) => {
+            return (
+              <Button
+                key={i}
+                className={btn === "CLEAR" ? "equals" : (btn === "ON/OFF" ? "onoff" : (numbArr.includes(btn) ? "numb" : ""))}
+                value={btn}
+                onClick={() => {
+                  console.log(`${btn} clicked!`);
+                }}
+              />
+            );
+          })
+        }
+      </ButtonBox>
+    </Wrapper>
   );
-}
+};
 
 export default App;
